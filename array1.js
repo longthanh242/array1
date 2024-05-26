703e8867d641d9c7db36ef623af94a4415834c97
longthanh242 <155937409+longthanh242@users.noreply.github.com>
2024-05-26 22:29:50 +0700

    Add files via upload

---
+++ array1.js
@@ -0,0 +1,82 @@
+// Bài 1: Cho một mảng có sẵn.
+// Tìm phần tử xuất hiện nhiều nhất trong mảng và in ra chúng
+
+
+
+
+            // let arr = prompt("Nhập vào 1 chuỗi số:");
+            // let arr1 = [...new Set(arr.split(""))];
+            // let arr2 = arr.split("");
+            // let arr3 = [];
+            // let count = 0;
+            // console.log('Arr1:', arr1);
+            // console.log('Arr2', arr2);
+            // let max = 0;
+            // let index=0;
+            // for (let i = 0; i < arr1.length; i++) {
+            //         for (var j = 0; j < arr2.length; j++) {
+            //                 if (arr1[i] === arr2[j]) {
+            //                     count++;
+            //                 }
+            //                 if (j===arr2.length-1) {
+            //                     arr3.push(count);
+            //                     count = 0;
+            //                 }
+            //         }
+            // }
+            // console.log('Arr3', arr3);
+
+
+            // for (let k = 0; k < arr3.length; k++) {
+            //     if (arr3[k] > max) {
+            //         max = arr3[k];
+            //         index=k;
+            //     } 
+            // }
+            // for (let l = 0; l < arr3.length; l++) {
+            //     if (arr3[l] === max) {
+            //         console.log(`Phần tử xuất hiện nhiều nhất trong mảng là: ${arr1[l]} với ${arr3[l]} lần`);
+            //     } 
+            // }
+
+
+
+
+// Bài 2: Loại bỏ các phần tử trùng nhau trong mảng
+// In ra mảng sau khi loại các phần tử giống nhau
+
+
+
+            // let arr = prompt('Nhập vào 1 chuỗi số:');
+            // let arr1 = arr.split('');
+
+            // for (let i = 0; i < arr1.length; i++) {
+            //     for (let j = i+1; j < arr1.length; j++) {
+            //         if (arr1[i] === arr1[j]) {
+            //             arr1.splice(j,1)
+            //             console.log(arr1);
+            //         }
+            //     }
+            // }
+
+            // console.log(arr1);
+
+
+
+// Bài 3: Viết chương trình nhập vào đoạn text
+// In ra text mới với chữ cái đầu tiên trong mỗi từ phải viết hoa
+
+
+
+            // let text = prompt("Nhập vào 1 chuỗi:");
+            // let text1 = text.split(' ');
+
+            // for (let i = 0; i < text1.length; i++) {
+            //     let word = text1[i];
+            //     if (word.length > 0) {
+            //         text1[i] = word.charAt(0).toUpperCase() + word.slice(1) + ' ';
+            //     }
+            // }
+            // document.write('Chuỗi sau khi sửa lại là:', text1.join(''));
+
+
